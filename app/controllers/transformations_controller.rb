c5813e0d655b6d759bcd8ba4ef12dcbbd83469c1
Diego Fejgelis <diegofej@gmail.com>
2016-12-28 12:30:21 -0300

    Added transformation model + controller

--- app/controllers/transformations_controller.rb
+++ app/controllers/transformations_controller.rb
@@ -1,2 +1,8 @@
+# Transformation Controller
 class TransformationsController < ApplicationController
+  def new; end
+
+  def create
+    render plain: params[:transformation].inspect
+  end
 end
---
+++ db/migrate/20161228150750_create_transformations.rb
@@ -0,0 +1,10 @@
+class CreateTransformations < ActiveRecord::Migration[5.0]
+  def change
+    create_table :transformations do |t|
+      t.string :title
+      t.text :text
+
+      t.timestamps
+    end
+  end
+end
