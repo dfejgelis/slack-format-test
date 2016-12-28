da820489871370452963d22931075568bb2d6523
Diego Fejgelis <diegofej@gmail.com>
2016-12-28 12:45:23 -0300

    Added index + show for Transformation

--- app/controllers/transformations_controller.rb
+++ app/controllers/transformations_controller.rb
@@ -1,8 +1,25 @@
 # Transformation Controller
 class TransformationsController < ApplicationController
+  def index
+    @transformations = Transformation.all
+  end
+
   def new; end
 
+  def show
+    @transformation = Transformation.find(params[:id])
+  end
+
   def create
-    render plain: params[:transformation].inspect
+    @transformation = Transformation.new(transformation_params)
+    @transformation.save
+
+    redirect_to @transformation
+  end
+
+  private
+
+  def transformation_params
+    params.require(:transformation).permit(:title, :text)
   end
 end
